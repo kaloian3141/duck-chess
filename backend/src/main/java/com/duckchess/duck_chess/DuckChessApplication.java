311b58a505932cbab198e9f391ac0529d226d7a0
kaloian3141 <kaloian70308@gmail.com>
2026-07-27 18:12:28 +0300

    adding spring boot backend

---
+++ backend/src/main/java/com/duckchess/duck_chess/DuckChessApplication.java
@@ -0,0 +1,13 @@
+package com.duckchess.duck_chess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuckChessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuckChessApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/duckchess/duck_chess/DuckChessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duckchess.duck_chess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuckChessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
